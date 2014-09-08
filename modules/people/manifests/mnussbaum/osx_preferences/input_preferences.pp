30d07b7f2556459a05234601445d4d4d199afb27
mnussbaum <michaelnussbaum08@gmail.com>
2014-09-07 21:46:27 -0500

    Lengthen key repeat delay to something reasonable

--- modules/people/manifests/mnussbaum/osx_preferences/input_preferences.pp
+++ modules/people/manifests/mnussbaum/osx_preferences/input_preferences.pp
@@ -1,13 +1,13 @@
 class people::mnussbaum::osx_preferences::input_preferences {
   include osx::global::enable_keyboard_control_access
   class { 'osx::global::key_repeat_delay':
-    delay => 4
+    delay => 10,
   }
   class { 'osx::global::key_repeat_rate':
-    rate => 1
+    rate => 1,
   }
   class { "osx::global::natural_mouse_scrolling":
-    enabled => true
+    enabled => true,
   }
   boxen::osx_defaults{ "Tap To Click":
     domain => "com.apple.mouse.tapBehavior",
